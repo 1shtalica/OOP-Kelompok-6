6ee2ca85086cdc23d2ed9f2a2ed312666e5a14ed
AdrianIchiro <adrianfahren10@gmail.com>
2024-04-16 23:53:56 +0700

    menambahkan fitur add mcu dan edit mcu pada pasien

--- DriverRumahSakit/src/main/java/user/Patient.java
+++ DriverRumahSakit/src/main/java/user/Patient.java
@@ -9,13 +9,16 @@ package user;
  * @author kevin
  */
 public class Patient extends User {
-   private String patientID;
-   private int age;
-    public Patient(int age ,String patientID,String username, String phoneNumber, UserType userType, String email) {
+
+    private String patientID;
+    private int age;
+    private PatientMcu[] mcu;
+    private int i = 0;
+
+    public Patient(int age, String patientID, String username, String phoneNumber, UserType userType, String email) {
         super(username, phoneNumber, userType, email);
         this.patientID = patientID;
         this.age = age;
-        
     }
 
     public String getPatientID() {
@@ -33,6 +36,21 @@ public class Patient extends User {
     public void setAge(int age) {
         this.age = age;
     }
-    
-    
+
+    public void addMcu(String date, String disease, String result) {
+        PatientMcu data = new PatientMcu(date, disease, result);
+        this.mcu[this.i] = data;
+        this.i++;
+    }
+
+    public void editMcu(int index, String date, String disease, String result) {
+        if (index >= 0 && index < this.i) {
+            this.mcu[index].setDate(date);
+            this.mcu[index].setDisease(disease);
+            this.mcu[index].setResult(result);
+        } else {
+            System.out.println("MCU not valid");
+        }
+    }
+
 }
